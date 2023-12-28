8f9f79ea519ca350d953941fb306b359747e96a4
Mohan Sai <80935489+mohansai123@users.noreply.github.com>
2023-12-28 09:08:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkins-s3-dynamo-bucket"
+    bucket         = "automated-s3-backend"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "jenkins-dynamodb"
+    dynamodb_table = "automate-dynamodb"
   }
 }
