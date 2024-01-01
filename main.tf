3f43580895b1c5a6072f7d21e8046bb511bdb942
Mohan Sai <80935489+mohansai123@users.noreply.github.com>
2024-01-01 18:37:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example_instance1" {
   ami                    = var.ami
   instance_type          = var.instance_type
   availability_zone      = var.availability_zone
@@ -11,5 +11,24 @@ resource "aws_instance" "example" {
   security_groups        = var.security_groups
   subnet_id              = var.subnet_id 
   associate_public_ip_address = true
-  tags                   = var.tags
+  tags                   = {
+    Name        = "Jenkins-Master"
+    Environment = "Dev"
+  }
 }
+
+resource "aws_instance" "example_instance2" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  availability_zone      = var.availability_zone
+  iam_instance_profile   = var.iam_instance_profile
+  key_name               = var.key_name
+  security_groups        = var.security_groups
+  subnet_id              = var.subnet_id 
+  associate_public_ip_address = true
+  tags                   = {
+    Name        = "Jenkins-Slave"
+    Environment = "Dev"
+  }
+}
+
