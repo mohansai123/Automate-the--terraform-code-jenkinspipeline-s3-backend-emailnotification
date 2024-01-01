d877e265a366029db79f7658d985a8ef24eed553
Mohan Sai <80935489+mohansai123@users.noreply.github.com>
2024-01-01 18:54:16 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "instance_ip" {
-  value = aws_instance.example.public_ip
+output "instance1_public_ip" {
+  value = aws_instance.example_instance1.public_ip
+}
+
+output "instance2_public_ip" {
+  value = aws_instance.example_instance2.public_ip
 }
